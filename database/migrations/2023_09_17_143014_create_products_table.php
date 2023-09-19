bc84035d570ce91df823ee4f9d7707f7476be507
matyo82 <matingarshasbii@gmail.com>
2023-09-19 12:35:13 +0330

    create project

---
+++ database/migrations/2023_09_17_143014_create_products_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            // name and title and description
+            $table->string('name');
+            $table->string('title');
+            $table->text('description');
+            // Sofa Features | ویژگی های مبل
+            $table->string('kalaf');
+            $table->string('fabric_color');
+            // چند مبل تک یادو یا سه نفر موجود است
+            $table->string('multi_one')->nullable();
+            $table->string('multi_two')->nullable();
+            $table->string('multi_three')->nullable();
+            // نوع اسفنح
+            $table->string('spongeـtype')->nullable();
+
+//            $table->string(); ضمانت
+
+            // options
+            $table->text('options');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
