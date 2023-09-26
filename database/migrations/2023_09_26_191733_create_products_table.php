c5cf686cf3de76070532c2afcbb3147231560c8c
matyo82 <matingarshasbii@gmail.com>
2023-09-26 22:56:59 +0330

    edite and create product migrations

--- database/migrations/2023_09_26_191733_create_products_table.php
+++ database/migrations/2023_09_26_191733_create_products_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,26 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+
+            // name and title and description
+            $table->string('name');
+            $table->string('title');
+            $table->text('description');
+            // Sofa Features | ویژگی های مبل
+            $table->string('kalaf');
+            $table->string('fabric_color');
+            // چند مبل تک یادو یا سه نفر موجود است
+            $table->string('multi_one')->nullable();
+            $table->string('multi_two')->nullable();
+            $table->string('multi_three')->nullable();
+            // نوع اسفنح
+            $table->string('spongeetype')->nullable();
+            // guarantee: 0 = no, 1 = yes
+            $table->enum('guarantee', [0, 1]);
+            // options
+            $table->text('options');
+
+
             $table->timestamps();
         });
     }
